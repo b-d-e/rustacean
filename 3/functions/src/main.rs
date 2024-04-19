1cd5d8482a4c8017ea32b254fbc0b3dc8d789dfa
Benjamin Etheridge <betheridge@turing.ac.uk>
2024-04-19 21:42:41 +0100

    3.3 functions

--- 3/functions/src/main.rs
+++ 3/functions/src/main.rs
@@ -1,29 +1,31 @@
-// main function is entry point of pogramme 
+// main function is entry point of pogramme
 fn main() {
     println!("Hello, world!");
     // call another function like this:
     another_function(); // note, doesn't have to come after the function def, and no concept of
                         // declaration. just needs to be defined in a scope the caller can see
     and_another(5); // pass argument
-                    
-    print_labeled_measurement(5, 'h'); // pass several arguments 
+
+    print_labeled_measurement(5, 'h'); // pass several arguments
 }
 
-fn another_function() { // we use snake case in function and variable names
+fn another_function() {
+    // we use snake case in function and variable names
     println!("Another function.");
 }
 
-fn and_another(x: i32) { // specify parameter, with it's type annotation
+fn and_another(x: i32) {
+    // specify parameter, with it's type annotation
     // you MUST provide type annotation in function parameters - this means compiler rarely forces
     // you to specify them elsewhere - it can use these to infer
     println!("The value of x is: {x}");
 }
 
-fn print_labeled_measurement(value: i32, unit_label: char) { // two parameters, each with their own
-                                                             // (different) type annotation)
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    // two parameters, each with their own
+    // (different) type annotation)
     println!("The measurement is: {value}{unit_label}");
 }
-
 
 // rust is made up of statements and expressions
 // statements are instructions that perform some action and do NOT return a value, i.e. let-ting a
@@ -37,7 +39,8 @@ fn print_labeled_measurement(value: i32, unit_label: char) { // two parameters, 
 // calling a function or a macro is an expression. a new scope block is an expression, returning a
 // value... e.g.
 fn example() {
-    let y = { // this {} is an expression
+    let y = {
+        // this {} is an expression
         let x = 3;
         x + 1 // note no semicolon - that would turn it into a statement at it wouldn't return a
               // value... be careful with this
@@ -46,14 +49,14 @@ fn example() {
     println!("The value of y is: {y}");
 }
 
-
 // returning values from functions
 // don't name return values, but MUST declare their type after an arrow in the function def
 // value retruend is synonymous with the value of the final expression
-// can return early with return keywork and specifying a value. generally, we return implicitly. 
+// can return early with return keywork and specifying a value. generally, we return implicitly.
 
-fn five() -> i32 { // specify return type with -> notation
-    5 // this is perfectly valid. 
+fn five() -> i32 {
+    // specify return type with -> notation
+    5 // this is perfectly valid.
 }
 
 fn notmain() {
