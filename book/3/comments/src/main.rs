b93f7e5d66b02a15af885cefd568f50f0dbe0eb6
Benjamin Etheridge <betheridge@turing.ac.uk>
2024-04-20 15:10:00 +0100

    3.4 comments

---
+++ book/3/comments/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // we comment above the line we're annotating, generally
+    println!("Hello, world!");
+}
+
+// single line comment
+
+/*
+ * we can also do block comments
+ * like this
+ * to document in more detail
+ * without lots of // newlines
+*/
+
+// rust also support documentation comments (like python docstrings)
+// this is used for things like Crates.io
